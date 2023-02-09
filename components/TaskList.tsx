4b41adcb1e5c8c4a8adff5af1d31d293905bacba
Phuc Nguyen <anicedream2002@gmail.com>
2023-02-09 11:37:41 -0600

    add TaskList component

---
+++ components/TaskList.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import {StyleSheet, FlatList} from 'react-native';
+import ITodo from '../models/todo.model';
+import TaskItem from './TaskItem';
+
+interface ITodoListProps {
+  data: ITodo[]
+}
+
+const TaskList = (props: ITodoListProps) => {
+  // const [isDone, setDone] = useState(false);
+  return (
+    <FlatList
+      style={styles.container}
+      data={props.data}
+      renderItem={(item: any) => {
+        return <TaskItem data={item.item} />;
+      }}
+      keyExtractor={item => item.id}
+    />
+  );
+};
+
+export default TaskList;
+
+const styles = StyleSheet.create({
+  container: {
+    height: 500,
+    width: '100%',
+    flexDirection: 'column',
+    padding: 10,
+    overflow: 'scroll',
+  },
+});
